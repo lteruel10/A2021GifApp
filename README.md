04cbf2809dc1a949a1b42fe08b28dfd03868597a
lteruel10 <lteruel10@gmail.com>
2021-09-24 17:52:13 -0600

    80. @ViewChild - Obtener referencias a objetos del HTML

--- README.md
+++ README.md
@@ -8,6 +8,21 @@
 # lo ideal es no tener practicamente nada en el app component
 
 
+# Non-null assertion operator
+# ViewChild   el simbolo ! nos ayuda a decirle a ts que el objeto no es nulo o asegurarle que existe.
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef;
+* ejemplo
+// decorador viewchild
+  //elementRef es un generico hay que decirle que tipo es para que no se any
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  buscar( ){
+  // elementRefe es un generico por eso es de tipo anY
+    const valor=this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value='';
+  }
+
 
 
 
--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -11,6 +11,17 @@ export class BusquedaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // decorado viewchild
+  //elementRef es un generico hay que decirle que tipo es para que no se any
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  buscar( ){
+    // elementRefe es un generico por eso es de tipo anY
+    const valor=this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value='';
   }
 
 }
